d69fd31ea213f4e46d02b52d830530cce47a2279
Juan van den Anker <juan@vdanker.net>
2017-07-18 14:27:39 +1200

    initial commit

---
+++ webpack-test/webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: [
+        "./src/index.js"
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        // publicPath: path.join(__dirname, "public"),
+        compress: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'src/test.html'
+        }),
+    ],
+};
